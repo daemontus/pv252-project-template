4c8f2184e44eb761cd77c522b99b1a68a6602e1b
Samuel Pastva <sam.pastva@gmail.com>
2024-10-07 11:46:15 +0200

    Add a bit more scaffolding to the lazy list solution.

--- src/entry_lazy.ts
+++ src/entry_lazy.ts
@@ -1,7 +1,11 @@
 import { initMenu } from "./menu.ts";
 import { LazyList } from "./lazy_list.ts";
+import { Person, loadFamousPeople, renderPerson } from "./famous_people.ts";
 
 LazyList.register();
 initMenu(document.querySelector<HTMLElement>(".main-menu")!, "menu-lazy-list");
+const list = document.querySelector<LazyList<Person>>("#lazy-list")!;
+list.setRenderer(renderPerson);
+list.setData(loadFamousPeople().slice(0, 100));
 
 console.log("Lazy site initialized...");
--- src/lazy_list.ts
+++ src/lazy_list.ts
@@ -7,12 +7,53 @@ div.list {
   border: var(--border);
   padding: var(--padding);
   overflow: scroll;
+  scrollbar-width: none;
+}
+#spacer-top {
+  width: 100%;
+  height: 0px;
+}
+#spacer-bottom {
+  width: 100%;
+  height: 1000px;
 }
 </style>
-<div class="list">My list content</div>
+<div id="list">
+  <div id="spacer-top"></div>
+  <slot></slot>
+  <div id="spacer-bottom"></div>
+</div>
 `;
 
-export class LazyList extends HTMLElement {
+export type Renderer<T> = (item: T) => HTMLElement;
+
+export class LazyList<T> extends HTMLElement {
+  // By default, the list renders the items as div-s with strings in them.
+  #renderFunction: Renderer<T> = (item) => {
+    const element = document.createElement("div");
+    element.innerText = JSON.stringify(item);
+    return element;
+  };
+
+  // These could be useful properties to consider, but not mandatory to use.
+  // Similarly, feel free to edit the shadow DOM template in any way you want.
+
+  // By default, the list is empty.
+  #data: T[] = [];
+
+  // The index of the first visible data item.
+  #visiblePosition: number = 0;
+
+  // The amount of space that needs to be shown before the first visible item.
+  #topOffset: number = 0;
+  #topOffsetElement: HTMLElement;
+  // The amount of space that needs to be shown after the last visible item.
+  #bottomOffset: number = 0;
+  #bottomOffsetElement: HTMLElement;
+
+  // The container that stores the spacer elements and the slot where items are inserted.
+  #listElement: HTMLElement;
+
   static register() {
     customElements.define("lazy-list", LazyList);
   }
@@ -24,9 +65,25 @@ export class LazyList extends HTMLElement {
   connectedCallback() {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.#topOffsetElement =
+      this.shadowRoot.querySelector<HTMLElement>("#spacer-top")!;
+    this.#bottomOffsetElement =
+      this.shadowRoot.querySelector<HTMLElement>("#spacer-bottom")!;
+    this.#listElement = this.shadowRoot.querySelector<HTMLElement>("#list")!;
+
+    this.#listElement.onscroll = () => {
+      console.log(this.#listElement.scrollTop);
+    };
   }
 
-  adoptedCallback() {
-    console.log("Custom element moved to new page.");
+  setData(data: T[]) {
+    this.#data = data;
+    // TODO: Data changed, re-draw content.
+  }
+
+  setRenderer(renderer: Renderer<T>) {
+    this.#renderFunction = renderer;
+    // TODO: Renderer changed, re-draw content.
   }
 }
